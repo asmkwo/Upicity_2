1fa87cfcd6292ca7b310dbe650b99709660ecfab
Marc de Vaugiraud <marc.devaugiraud@edu.esiee.fr>
2022-04-04 14:51:07 +0200

    :zap: map working again :sparkles: table is displaying, allows to save points coordinates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy  import SQLAlchemy
 from datetime import datetime
 
@@ -19,9 +19,21 @@ db.create_all()
 @app.route('/',methods=['POST','GET'])
 def index():
     if request.method == 'POST':
-        pass
+        position_coord = request.form['content']
+        new_point = Point(content=position_coord)
+        try :
+            db.session.add(new_point)
+            db.session.commit()
+            return redirect('/')
+        except :
+            return 'There was an issue adding your point'
     else :
-        return render_template('index.html')
+        points = Point.query.order_by(Point.date_created).all()
+        return render_template('index.html', points=points)
+
+@app.route
+
+
 
 
 if __name__ == '__main__':
